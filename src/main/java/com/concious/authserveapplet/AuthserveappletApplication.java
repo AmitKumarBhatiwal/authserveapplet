d7c03136f5b6462e134ae6bd4be5f9b910b40530
Amit kumar <amit.kumar@naskay.in>
2023-04-01 00:08:07 +0530

    Initial commit

---
+++ src/main/java/com/concious/authserveapplet/AuthserveappletApplication.java
@@ -0,0 +1,13 @@
+package com.concious.authserveapplet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthserveappletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthserveappletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/concious/authserveapplet/AuthserveappletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concious.authserveapplet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthserveappletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
